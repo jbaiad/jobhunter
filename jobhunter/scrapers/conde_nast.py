6739aefca045ec61aabb46f04a817d52f0a01b43
Josh Baiad <josh@joshbaiad.com>
2019-07-30 14:45:28 -0400

    perf(scrapers): use requests for Workday scrapers
    After taking a look at the network traffic in Chrome, it became clear
    that we can replace Selenium with explicit GET requests.

--- jobhunter/scrapers/conde_nast.py
+++ jobhunter/scrapers/conde_nast.py
@@ -1,10 +1,5 @@
-import selenium.webdriver as selenium_web_drivers
-
 from jobhunter.scrapers.interfaces import AbstractWorkdayScraper
 
 
 class CondeNastScraper(AbstractWorkdayScraper):
-    _DRIVER_OPTIONS = selenium_web_drivers.ChromeOptions()
-    _DRIVER_OPTIONS.add_argument('--headless')
-    DRIVER = selenium_web_drivers.Chrome(chrome_options=_DRIVER_OPTIONS)
     ROOT_URL = r'https://condenast.wd5.myworkdayjobs.com/CondeCareers'
--- jobhunter/scrapers/interfaces.py
+++ jobhunter/scrapers/interfaces.py
@@ -1,13 +1,9 @@
 import abc
+import json
 
+import bs4
 import pandas as pd
-import selenium.webdriver as selenium_web_drivers
-from selenium.webdriver.common.by import By
-from selenium.webdriver.remote.webdriver import WebDriver
-import selenium.webdriver.support.expected_conditions as selenium_conditions
-from selenium.webdriver.support.ui import WebDriverWait
-
-import jobhunter.utils.selenium as selenium_utils
+import requests
 
 
 class AbstractScraper(abc.ABC):
@@ -23,64 +19,63 @@ class AbstractMetaWorkdayScraper(abc.ABCMeta):
     def __new__(mcs, name, bases, namespace):
         if not name.upper().startswith('ABSTRACT'):
             assert isinstance(namespace.get('ROOT_URL'), str)
-            assert isinstance(namespace.get('DRIVER'), WebDriver)
         return super().__new__(mcs, name, bases, namespace)
 
 
 class AbstractWorkdayScraper(AbstractScraper,
                              metaclass=AbstractMetaWorkdayScraper):
-    @classmethod
-    def _load_job_listings(cls):
-        cls.DRIVER.get(cls.ROOT_URL)
-        pause_driver = WebDriverWait(cls.DRIVER, 10)
-        page_ready = selenium_conditions.presence_of_element_located(
-            (By.CSS_SELECTOR, 'div[aria-label="Search Results"]')
-        )
-        pause_driver.until(page_ready)
-        selenium_utils.load_page_with_infinite_scroll(cls.DRIVER, 1.5)
-
-        return cls.DRIVER.find_elements_by_class_name('WNYF.WLYF')
-
-    @classmethod
-    def _fetch_job_url(cls, job_listing):
-        button = job_listing.find_element_by_css_selector('div[role="link"]')
-        action_chain = selenium_web_drivers.ActionChains(cls.DRIVER)
-        action_chain.context_click(button).perform()
-        copy_url_button = cls.DRIVER.find_element_by_css_selector(
-            'div[title="Copy URL"]'
-        )
-
-        return copy_url_button.get_attribute('data-clipboard-text')
-
-    @classmethod
-    def _fetch_job_info(cls, job_url):
-        cls.DRIVER.get(job_url)
-        pause_driver = WebDriverWait(cls.DRIVER, 10)
-        page_ready = selenium_conditions.presence_of_element_located(
-            (By.CLASS_NAME, 'WIOO')
-        )
-        pause_driver.until(page_ready)
-
-        job_info_elements = [
-            x.text for x in cls.DRIVER.find_elements_by_class_name('WMMO')
-        ]
-        job_info = {
-            'title': job_info_elements[0],
-            'location': '; '.join(sorted(job_info_elements[1:-6])),
-            'description': job_info_elements[-6],
-            'posted': job_info_elements[-5],
-            'type': job_info_elements[-4],
-            'url': job_url,
-        }
-
-        return job_info 
+    SEARCH_ENDPOINT = 'fs/searchPagination/318c8bb6f553100021d223d9780d30be'
 
     @classmethod
     def scrape(cls, writer: any = None) -> pd.DataFrame:
-        job_listings = cls._load_job_listings()
-        job_urls = [cls._fetch_job_url(listing) for listing in job_listings]
-        jobs = pd.DataFrame([cls._fetch_job_info(url) for url in job_urls])
+        return pd.DataFrame([
+            cls._get_job_info(f'{cls.ROOT_URL}/{endpoint}')
+            for endpoint in cls._fetch_job_endpoints()
+        ])
 
-        cls.DRIVER.quit()
+    @classmethod
+    def _get_job_info(cls, job_url: str) -> dict:
+        response = requests.get(job_url, headers={'Accept': 'application/json'})
+        info = response.json()['structuredDataAttributes']['data']
+        info = json.loads(info) 
 
-        return jobs
+        info['date_posted'] = info['datePosted']
+        info['employment_type'] = info['employmentType']
+        info['location'] = info['jobLocation']['address']['addressLocality']
+        info['url'] = job_url
+
+        del info['@context']
+        del info['@type']
+        del info['hiringOrganization']
+        del info['identifier']
+        del info['datePosted']
+        del info['employmentType']
+        del info['jobLocation']
+
+        return info
+
+    @classmethod
+    def _fetch_job_endpoints(cls) -> list:
+        offset = 0
+        job_endpoints = []
+        jobs = cls._get_search_results(offset)
+
+        while len(jobs) > 0:
+            offset += len(jobs)
+            job_endpoints.extend([
+                '/'.join(j['title']['commandLink'].split('/')[3:]) for j in jobs
+            ])
+            jobs = cls._get_search_results(offset)
+
+        print(f'Found {len(job_endpoints)} jobs')
+
+        return job_endpoints
+    
+    @classmethod
+    def _get_search_results(cls, offset: int) -> dict:
+        url = f'{cls.ROOT_URL}/{cls.SEARCH_ENDPOINT}/{offset}'
+        response = requests.get(url).json()
+        containing_json = response['body']['children'][0]['children'][0]
+
+        return containing_json.get('listItems', [])
+
