85f5fa6a1672adb1ea1039f53c888a1acfdd633c
Josh Baiad <josh@joshbaiad.com>
2019-07-30 21:55:08 -0400

    fix(scrapers): fix typo

--- jobhunter/scrapers/conde_nast.py
+++ jobhunter/scrapers/conde_nast.py
@@ -2,7 +2,7 @@ from jobhunter.scrapers.interfaces import AbstractWorkdayScraper
 
 
 class CondeNastScraper(AbstractWorkdayScraper):
-    COMAPNY_NAME = 'Condé Nast'
+    COMPANY_NAME = 'Condé Nast'
     ROOT_URL = r'https://condenast.wd5.myworkdayjobs.com/CondeCareers'
 
 __all__ = ['CondeNastScraper']
