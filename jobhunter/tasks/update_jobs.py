95c730c3f7e10038132b6b272e6f4d708fe94b4a
Josh Baiad <josh@joshbaiad.com>
2019-10-27 23:33:33 -0400

    feat(airflow): add Hearst scraper task to airflow

--- jobhunter/tasks/update_jobs.py
+++ jobhunter/tasks/update_jobs.py
@@ -16,7 +16,7 @@ DEFAULT_ARGS = {
     'email_on_retry': False,
     'retries': 1,
     'retry_delay': dt.timedelta(minutes=2),
-    'start_date': utils.timezone.datetime(2019, 10, 20, 22)
+    'start_date': utils.timezone.datetime(2019, 10, 27, 22)
 }
 
 with airflow.DAG('job_scrapers', default_args=DEFAULT_ARGS, schedule_interval=dt.timedelta(minutes=10)) as scraper_dag:
@@ -31,4 +31,8 @@ with airflow.DAG('job_scrapers', default_args=DEFAULT_ARGS, schedule_interval=dt
     python_operator.PythonOperator(
         task_id='nyt',
         python_callable=lambda: scrapers.NewYorkTimesScraper.scrape(daos.JobWriter)
+    )
+    python_operator.PythonOperator(
+        task_id='hearst',
+        python_callable=lambda: scrapers.HearstScraper.scrape(daos.JobWriter)
     )
